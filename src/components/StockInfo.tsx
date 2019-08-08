80a5653039f8f58c92e21e2778cae7198fc23d58
Sean Fahy <Seanfahy45@gmail.com>
2019-08-08 21:28:41 +0100

    simplified the StockInfo component's code and made each stock's card responsive

--- src/components/StockInfo.tsx
+++ src/components/StockInfo.tsx
@@ -54,36 +54,30 @@ const StockInfo: React.FC<IProps> = (props: IProps) => {
             />
           </Card.Header>
           <div id="stockInfo">
-            <div>
-              <Card.Description>
-                Current price: {accounting.formatMoney(apiData.latestPrice)}
-              </Card.Description>
-              <Card.Description>
-                Market Cap:{" "}
-                {accounting.formatMoney(apiData.marketCap, {
-                  precision: 0
-                })}
-              </Card.Description>
-            </div>
-            <div>
-              <Card.Description>
-                P/E ratio: {apiData.peRatio.toFixed(1)}
-              </Card.Description>
-              <Card.Description>
-                52 week high: {accounting.formatMoney(apiData.week52High)}
-              </Card.Description>
-            </div>
-            <div>
-              <Card.Description>
-                52 week low: {accounting.formatMoney(apiData.week52Low)}
-              </Card.Description>
-              <Card.Description>
-                Holdings Value:{" "}
-                {accounting.formatMoney(apiData.latestPrice * props.quantity, {
-                  precision: 0
-                })}
-              </Card.Description>
-            </div>
+            <Card.Description>
+              Current price: {accounting.formatMoney(apiData.latestPrice)}
+            </Card.Description>
+            <Card.Description>
+              Market Cap:{" "}
+              {accounting.formatMoney(apiData.marketCap, {
+                precision: 0
+              })}
+            </Card.Description>
+            <Card.Description>
+              P/E ratio: {apiData.peRatio.toFixed(1)}
+            </Card.Description>
+            <Card.Description>
+              52 week high: {accounting.formatMoney(apiData.week52High)}
+            </Card.Description>
+            <Card.Description>
+              52 week low: {accounting.formatMoney(apiData.week52Low)}
+            </Card.Description>
+            <Card.Description>
+              Holdings Value:{" "}
+              {accounting.formatMoney(apiData.latestPrice * props.quantity, {
+                precision: 0
+              })}
+            </Card.Description>
           </div>
         </Card.Content>
       </Card>
